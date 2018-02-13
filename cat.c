0a7e9914f9edde1edc1717053543d3ef4b1aa480
yinm <yinm.cminor9@gmail.com>
2018-02-13 23:17:01 +0900

    Check 2nd

--- cat.c
+++ cat.c
@@ -1,3 +1,4 @@
+// 2nd
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,13 +13,16 @@ int
 main(int argc, char *argv[])
 {
   int i;
+
   if (argc < 2) {
     fprintf(stderr, "%s: file name not given\n", argv[0]);
     exit(1);
   }
+
   for (i = 1; i < argc; i++) {
     do_cat(argv[i]);
   }
+
   exit(0);
 }
 
@@ -39,6 +43,7 @@ do_cat(const char *path)
     if (n == 0) break;
     if (write(STDOUT_FILENO, buf, n) < 0) die(path);
   }
+
   if (close(fd) < 0) die(path);
 }
 
