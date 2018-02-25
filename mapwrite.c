9dfd089df921759a8bec60c0abd8e3aeda716bf2
yinm <yinm.cminor9@gmail.com>
2018-02-25 13:12:18 +0900

    Check mmap()

---
+++ mapwrite.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#define DATAFILE "mapwrite.data"
+
+int
+main(int argc, char *argv[])
+{
+  int fd;
+  char *ptr;
+  int len = 3;
+
+  unlink(DATAFILE);
+  fd = open(DATAFILE, O_WRONLY|O_CREAT, 0666);
+
+  if (fd < 0) {
+    perror(DATAFILE);
+    exit(1);
+  }
+
+  write(fd, "NO\n", 3);
+  close(fd);
+
+  fd = open(DATAFILE, O_RDWR);
+  if (fd < 0) {
+    perror(DATAFILE);
+    exit(1);
+  }
+  ptr = (char*)mmap(0, len, PROT_WRITE, MAP_SHARED, fd, 0);
+  if (!ptr) {
+    perror("mmap(2)");
+    exit(1);
+  }
+  close(fd);
+  ptr[0] = 'O';
+  ptr[1] = 'K';
+  ptr[2] = '\n';
+  munmap(ptr, len);
+
+  system("cat " DATAFILE);
+
+  exit(0);
+}
