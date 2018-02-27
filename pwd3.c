4228cfd22c486e2d1cdf809853f20a69feea8451
yinm <yinm.cminor9@gmail.com>
2018-02-27 23:42:28 +0900

    Check getcwd()

---
+++ pwd3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+    char *path;
+
+    path = getcwd(NULL, 0);
+
+    if (!path) {
+        perror(path);
+        exit(1);
+    }
+    puts(path);
+    free(path);
+    exit(0);
+}
