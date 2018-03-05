07efc38aa52e1c45bab8b995e0505c51a113cbd2
yinm <yinm.cminor9@gmail.com>
2018-03-06 08:51:08 +0900

    Check 2nd

--- isatty.c
+++ isatty.c
@@ -1,3 +1,4 @@
+// 2nd
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,7 +6,7 @@
 int
 main(int argc, char *argv[])
 {
-  printf("stdin: %d\n", isatty(0));
+  printf("stdin:  %d\n", isatty(0));
   printf("stdout: %d\n", isatty(1));
   printf("stderr: %d\n", isatty(2));
   exit(0);
