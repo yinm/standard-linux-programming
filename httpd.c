6ce42184b901ebb149a1b03653292267634d8707
yinm <yinm.cminor9@gmail.com>
2018-03-07 23:27:13 +0900

    Check signal process

--- httpd.c
+++ httpd.c
@@ -12,6 +12,34 @@
 #include <ctype.h>
 #include <signal.h>
 
+/****** Function Prototypes **********************************************/
+
+typedef void (*sighandler_t)(int);
+
+static void
+install_signal_handlers(void)
+{
+    trap_signal(SIGPIPE, signal_exit);
+}
+
+static void
+trap_signal(int sig, sighandler_t handler)
+{
+    struct sigaction act;
+
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_RESTART;
+    if (sigaction(sig, &act, NULL) < 0)
+        log_exit("sigaction() failed: %s", strerror(errno));
+}
+
+static void
+signal_exit(int sig)
+{
+    log_exit("exit by signal %d", sig);
+}
+
 static void*
 xmalloc(size_t sz)
 {
