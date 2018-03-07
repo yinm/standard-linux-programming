f1062d6b412cb9d0b86abde7b687209afe111353
yinm <yinm.cminor9@gmail.com>
2018-03-08 08:04:38 +0900

    Add free_request()

--- httpd.c
+++ httpd.c
@@ -68,6 +68,27 @@ service(FILE *in, FILE *out, char *docroot)
     free_request(req);
 }
 
+
+static void
+free_request(struct HTTPRequest *req)
+{
+    struct HTTPHeaderField *h, *head;
+
+    head = req->header;
+    while (head) {
+        h = head;
+        head = head->next;
+        free(h->name);
+        free(h->value);
+        free(h);
+    }
+    free(req->method);
+    free(req->path);
+    free(req->body);
+    free(req);
+}
+
+
 static void*
 xmalloc(size_t sz)
 {
