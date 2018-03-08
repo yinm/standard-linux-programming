72b3cf65175ba306b6045296da12f3dc6e3a191e
yinm <yinm.cminor9@gmail.com>
2018-03-09 08:27:58 +0900

    Add listen_socket

--- httpd2.c
+++ httpd2.c
@@ -160,3 +160,38 @@ main(int argc, char *argv[])
 }
 
 
+static int
+listen_socket(char *port)
+{
+  struct addrinfo hints, *res, *ai;
+  int err;
+
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  if ((err = getaddrinfo(NULL, port, &hints, &res)) != 0)
+    log_exit(gai_strerror(err));
+
+  for (ai = res; ai; ai = ai->ai_next) {
+    int sock;
+
+    sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+    if (sock < 0) continue;
+    if (bind(sock, ai->ai_addr, ai->ai_addrlen) < 0) {
+      close(sock);
+      continue;
+    }
+    if (listen(sock, MAX_BACKLOG) < 0) {
+      close(sock);
+      continue;
+    }
+
+    freeaddrinfo(res);
+    return sock;
+  }
+
+  log_exit("failed to listen socket");
+  return -1; // NOT REACH
+}
+
